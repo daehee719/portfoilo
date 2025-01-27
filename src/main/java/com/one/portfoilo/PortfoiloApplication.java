a62e3c10854eeee18558a0e4852bdb4f17ac4a79
daehee719 <daeheuigang@gmail.com>
2025-01-28 00:27:03 +0900

    유저 로그인 처리

--- src/main/java/com/one/portfoilo/PortfoiloApplication.java
+++ src/main/java/com/one/portfoilo/PortfoiloApplication.java
@@ -3,7 +3,7 @@ package com.one.portfoilo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.one.portfoilo")
 public class PortfoiloApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/one/portfoilo/domain/repository/UserRepository.java
+++ src/main/java/com/one/portfoilo/domain/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.one.portfoilo.domain.repository;
 import com.one.portfoilo.domain.SocialType;
 import com.one.portfoilo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
