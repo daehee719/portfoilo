2662a466c81e7ce3190897c1299ed95e6ce0db78
daehee719 <daeheuigang@gmail.com>
2025-01-27 22:47:44 +0900

    init

---
+++ src/main/java/com/one/portfoilo/PortfoiloApplication.java
@@ -0,0 +1,13 @@
+package com.one.portfoilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoiloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfoiloApplication.class, args);
+    }
+
+}
